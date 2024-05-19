3b97b995dacd31dc207e40f0cd72b121ed8ab943
Abhijeet-Khavaskar <abhi.625khavaskar@gmail.com>
2024-05-19 23:42:09 +0530

    xcountreis initial commit done

---
+++ xcountries/src/App.js
@@ -0,0 +1,12 @@
+import Xcountries from "./Components/Xcountries"
+import './App.css';
+
+function App() {
+  return (
+    <>
+<Xcountries />
+    </>
+  );
+}
+
+export default App;
---
+++ xcountries/src/Components/Xcountries.js
@@ -0,0 +1,10 @@
+import styles from "./Xcountries.module.css"
+
+export default function Xcountries(){
+    
+    return (
+        <>
+xcountries
+        </>
+    )
+}
